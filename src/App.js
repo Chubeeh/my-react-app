ac37d55909f8e15b60407f5eb612f09d9c9d8f13
Chubeeh <amehemmanuella1@gmail.com>
2024-04-05 14:36:04 -0700

    Added another link

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>
-      Welcome
+      Welcome app
      </h1>
-     <p>This was coded by <a href ="#">Ella Ameh</a>, open-sourced on <a href ="https://github.com/Chubeeh/react-project" 
-     target='blank'>Github</a> and hosted on Vercel</p>
+     <p>This was coded by <a href ="#">Ella Ameh</a>, open-sourced on <a href ="https://github.com/Chubeeh/my-react-app" 
+     target='blank'>Github</a> and hosted on <a href='' target='blank'>Vercel</a></p>
     </div>
   );
 }
