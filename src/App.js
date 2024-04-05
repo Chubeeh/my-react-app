d9178680fa96f2c1f2ef25b8869b69bdb6e54352
Chubeeh <amehemmanuella1@gmail.com>
2024-04-05 14:30:08 -0700

    Added react app and removed unnecessary codes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+      Hello dear
+     </h1>
+     <p>This was coded by <a href ="#">Ella Ameh</a>, open-sourced on <a href ="https://github.com/Chubeeh/react-project" 
+     target='blank'>Github</a> and hosted on Vercel</p>
     </div>
   );
 }
