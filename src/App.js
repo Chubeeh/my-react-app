6d44cb59ccf016b4b62267f572e285e261aa2083
Chubeeh <amehemmanuella1@gmail.com>
2024-04-10 10:31:17 -0700

    Updated the CSS

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1 >
-      Weather Forecast
+        <h1 className='heading'>
+      Today's Weather Forecast
      </h1>
-     <Weather defaultCity="New zealand"/>
-     <p>This was coded by <a href ="https://my-react-app-six-puce.vercel.app/" target='blank'>Ella Ameh</a>, open-sourced on <a href ="https://github.com/Chubeeh/my-react-app" 
+     <Weather defaultCity="Botswana"/>
+     <footer>
+     <p className='last-text'>This was coded by <a href ="https://my-react-app-six-puce.vercel.app/" target='blank'>Ella Ameh</a>, open-sourced on <a href ="https://github.com/Chubeeh/my-react-app" 
      target='blank'>Github</a> and hosted on <a href='https://my-react-app-six-puce.vercel.app/' target='blank'>Vercel</a></p>
+     </footer>
     </div>
     </div>
   
