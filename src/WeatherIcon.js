66c86d530c33e20488f67091f9a2d3bff7795146
Chubeeh <amehemmanuella1@gmail.com>
2024-04-10 07:09:15 -0700

    Added fahrenheit to the temperature

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return ( <ReactAnimatedWeather
     icon={codeMapping[props.code]}
     color="#FFE168"
-    size={60}
+    size={50}
     animate={true}
   />
 );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PresentedDate from "./PresentedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
     return(
@@ -10,25 +11,20 @@ export default function WeatherInfo(props){
             <div className="row mt-3">
                 <div className="col-8">
                     <p className="text-muted">
-   <span className="Weather-description">
-  <PresentedDate date={props.data.date} />, {props.data.description}
-</span>
-  <span className="Weather-description">Humidity:<span className="red">{props.data.humidity}%</span>, Wind:<span className="red"> {props.data.wind}km/h</span>
- 
-</span>
-   
-  </p>       
-                    </div>
+                        <span className="Weather-description">
+                            <PresentedDate date={props.data.date} />, {props.data.description}
+                        </span>
+                        <span className="Weather-description">Humidity:<span className="red">{props.data.humidity}%</span>, Wind:<span className="red"> {props.data.wind}km/h</span></span>
+                    </p>       
+                </div>
                 <div className="col-4 d-flex justify-content-end">
                     <div className="img-box">
                         <span className="main-img">
-                           <WeatherIcon code={props.data.icon} />  
-                </span>
-                <span className="temp-value"><strong>{Math.round(props.data.temperature)}</strong></span>
-               
-                <span className="temp-unit">°C</span>
+                            <WeatherIcon code={props.data.icon} />  
+                        </span>
+                    </div>
+                    <WeatherTemperature celsius={props.data.temperature} />
                 </div>
-                 </div>
             </div>
        </div>
     );
